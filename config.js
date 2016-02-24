282de4bf202a979ee0583663f3910850dbe65568
remyareghu <remyar@qburst.com>
2016-02-24 11:11:56 +0530

    changes

--- config.js
+++ config.js
@@ -18,16 +18,4 @@ angularApp.config(function ($stateProvider, $urlRouterProvider) {
         
 });
 
-//myApp.config([ '$routeProvider', function($routeProvider) {
-//	$routeProvider
-
-//	.when('/vacation', {
-//		templateUrl : '/views/vacation.html',
-//		controller : 'VacationController'
-//	})
-	
-//	.otherwise({
-//		redirectTo : '/'
-//	});
-//} ]);
 
