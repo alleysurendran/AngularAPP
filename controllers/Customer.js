f937fa608069a72c67f3935080bb1bbaca304d5b
alleysurendran <alley@qburst.com>
2016-02-25 17:34:11 +0530

    style changes

--- controllers/Customer.js
+++ controllers/Customer.js
@@ -1,10 +1,8 @@
 ﻿angularApp.controller('CustomerController', ['$scope', '$http', '$filter', function ($scope, $http, $filter) {
     BindCustomer();
     function BindCustomer() {
-        alert('bind');
                 $http.get("shared/json/Customer.JSON")
                 .then(function (response) {
-                    alert('hi');
                     $scope.customerDetails = response.data;
                 });
 
