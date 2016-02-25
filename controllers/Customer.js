2bca2b21e08a8361b3c952e535bc5c0b706395ac
alleysurendran <alley@qburst.com>
2016-02-25 16:46:12 +0530

    customer change

--- controllers/Customer.js
+++ controllers/Customer.js
@@ -1,9 +1,14 @@
-﻿angularApp.controller('CustomerController', function ($scope,$http) {
-        $http.get("shared/json/Customer.JSON")
-        .then(function (response) {
-            $scope.customerDetails = response.data;
-           // alert($scope.customerDetails);
-        });
-       
-});
+﻿angularApp.controller('CustomerController', ['$scope', '$http', '$filter', function ($scope, $http, $filter) {
+    BindCustomer();
+    function BindCustomer() {
+        alert('bind');
+                $http.get("shared/json/Customer.JSON")
+                .then(function (response) {
+                    alert('hi');
+                    $scope.customerDetails = response.data;
+                });
+
+        }
+   
+}]);
 
