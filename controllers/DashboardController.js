39519d8bada399063aa6423b9f9b9160e7fb66b0
divyaremadevi <divyar@qburst.com>
2016-02-26 16:51:07 +0530

    Logout

--- controllers/DashboardController.js
+++ controllers/DashboardController.js
@@ -1,9 +1,10 @@
 ﻿angularApp.controller('DashboardController', ['$scope', '$rootScope', 'LoginVaildationService', 'LogoutService', function ($scope, $rootScope, userSession, userLogout) {
 
    //*******************To set Sign Out Link and User Name in Header*****************************************//
-       if (userSession.isLogged) {
-        $rootScope.showUser = true;
+    if (userSession.isLogged) {
+        
         $rootScope.userName = userSession.username;
+        $rootScope.showUser = true;
         $rootScope.sidebar = true;
     }
 }]);
