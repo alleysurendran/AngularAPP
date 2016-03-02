bdaa7a22687120d3ca178f9151fce50b92ee58ae
remyareghu <remyar@qburst.com>
2016-03-02 13:24:41 +0530

    Modified Json get service

--- services/JSONService.js
+++ services/JSONService.js
@@ -1,16 +1,5 @@
 ﻿
-angularApp.service('JSONService', function ($http) {
 
-    var jsonResult = {};
-    jsonResult.GetJsonValue = function (filepath) {
 
-        $http.get(filepath)
-       .then(function (response) {
-           jsonResult = response.data;
-       });
-        return jsonResult;
 
-    }
 
-    return jsonResult;
-});
